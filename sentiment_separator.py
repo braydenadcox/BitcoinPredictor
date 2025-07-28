5f185919348a16e6eafcfe60046f302df8d0a547
Brayden Adcox <braydenadcox@gmail.com>
2025-07-27 18:12:55 -0700

    Added score-only roberta sentiment file

--- sentiment_separator.py
+++ sentiment_separator.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('data/distilbert_finetuned_sentiment.csv')
+df = pd.read_csv('data/roberta_sentiment.csv')
 
 single = df[['sentiment_score']]
 
-single.to_csv('data/distilbert_finetuned_influence_score_only.csv', index=False)
+single.to_csv('data/roberta_influence_score_only.csv', index=False)
